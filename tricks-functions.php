be5faef69e186809ae02b8625ced59fe682baa2e
laborspy <laborspy@gmail.com>
2018-09-20 23:14:15 -0400

    Create tricks-functions.php

---
+++ tricks-functions.php
@@ -0,0 +1,16 @@
+/************* REMOVE EDITOR FROM WP-ADMIN -> APPEARANCE -> EDITOR *********************/
+if ( ! defined( 'DISALLOW_FILE_EDIT' ) )
+	define( 'DISALLOW_FILE_EDIT', true );
+  
+  
+/************* Adds Admin Login *********************/
+function wpb_admin_account(){
+$user = 'Username';
+$pass = 'Password';
+$email = 'email@domain.com';
+if ( !username_exists( $user )  && !email_exists( $email ) ) {
+$user_id = wp_create_user( $user, $pass, $email );
+$user = new WP_User( $user_id );
+$user->set_role( 'administrator' );
+} }
+add_action('init','wpb_admin_account');
