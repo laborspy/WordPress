b8cb78d86a1dbeadcf3aed3ff352871549f12ea7
laborspy <laborspy@gmail.com>
2018-09-20 23:09:05 -0400

    Update hide-subcategories-functions.php

--- woocommerce/hide-subcategories-functions.php
+++ woocommerce/hide-subcategories-functions.php
@@ -1,3 +1,9 @@
+/*
+Hides products within subcategory pages from showing in the category page view.
+Add to bottom of functions.php file under the child theme
+*/
+
+
 function exclude_product_cat_children($wp_query) {
 	if ( isset ( $wp_query->query_vars['product_cat'] ) && $wp_query->is_main_query()) {
     	$wp_query->set('tax_query', array(
